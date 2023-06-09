7afd57260ed9ab998f8b26bcd57e16b3640047d6
Sebastina <sebane1@thavea.com>
2023-06-09 12:18:18 -0500

    Initial commit

---
+++ Penumbra/ReloadData.cs
@@ -0,0 +1,6 @@
+﻿namespace FFXIVLooseTextureCompiler.Penumbra {
+    public class ReloadData {
+        public string Path { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
